b3a8241c92a098cc98de1d8fa50643722f9a885d
Kurt Krafft <kkrafft6609@gmail.com>
2020-04-21 15:26:33 -0500

    arb

--- plants/plant.py
+++ plants/plant.py
@@ -3,3 +3,6 @@ class Plant:
     def __init__(self, species, season):
       self.species = species
       self.peak_season = season
+    
+    def add_plant(self, habitat):
+      habitat.plants.append(self)
