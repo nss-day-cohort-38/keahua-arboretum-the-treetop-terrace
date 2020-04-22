3e0411deb4b1657e7317409b7a732dd36b558a7d
Jack Parsons <jcksnparsons@yahoo.com>
2020-04-22 14:59:01 -0500

    reformat report again

--- actions/report.py
+++ actions/report.py
@@ -3,16 +3,20 @@ from arboretum import Arboretum
 def build_facility_report(Arboretum):
     for environments in Arboretum.master:
         for environment in environments:
-            print(environment)
+            print(f'''     {environment}''')
             print('''
-            ----- ANIMALS -----
+                    ----- ANIMALS -----
             ''')
             for animal in environment.animals:
                 print(animal)
             print('''
-            ----- PLANTS ------
+                    ----- PLANTS ------
              ''')
             for plant in environment.plants:
                 print(plant)
+            
+    print('''
+                ------  END REPORT  -------
+        ''')
 
     input("\n\nPress any key to continue...")
