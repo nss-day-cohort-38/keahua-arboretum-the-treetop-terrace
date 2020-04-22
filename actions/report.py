ccdc1ba6ded05b155fa9035e89146043620c900a
Jack Parsons <jcksnparsons@yahoo.com>
2020-04-22 14:49:39 -0500

    reorganized the report function

--- actions/report.py
+++ actions/report.py
@@ -4,8 +4,14 @@ def build_facility_report(Arboretum):
     for environments in Arboretum.master:
         for environment in environments:
             print(environment)
+            print('''
+            ----- ANIMALS -----
+            ''')
             for animal in environment.animals:
                 print(animal)
+            print('''
+            ----- PLANTS ------
+             ''')
             for plant in environment.plants:
                 print(plant)
 
