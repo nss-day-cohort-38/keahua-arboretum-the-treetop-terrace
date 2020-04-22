2d5c1632f1be3ca598f1e95c9dcdb2ccc4284957
Michael Clark <michaelclarknashville@protonmail.com>
2020-04-22 13:54:54 -0500

    annex habitat working

--- actions/annex.py
+++ actions/annex.py
@@ -1,6 +1,26 @@
+# import os
+# from environments import River
+# # from index import build_menu
+
+# def annex_habitat(arboretum):
+#     os.system('cls' if os.name == 'nt' else 'clear')
+#     print("1. River")
+#     print("2. Swamp")
+#     print("3. Coastline")
+#     print("4. Grassland")
+
+#     choice = input("Choose your habitat > ")
+
+#     if choice == "1":
+#         river = River()
+#         arboretum.rivers.append(river)
+#         print('river added')
+#         # build_menu()
+#     if choice == "2":
+#         pass
+
 import os
-from environments import River
-# from index import build_menu
+from environments import River, Swamp, Coastline, Grassland
 
 def annex_habitat(arboretum):
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -14,7 +34,12 @@ def annex_habitat(arboretum):
     if choice == "1":
         river = River()
         arboretum.rivers.append(river)
-        print('river added')
-        # build_menu()
     if choice == "2":
-        pass
+        swamp = Swamp()
+        arboretum.swamps.append(swamp)
+    if choice == "3":
+        coastline = Coastline()
+        arboretum.coastlines.append(coastline)
+    if choice == "4":
+        grassland = Grassland()
+        arboretum.grasslands.append(grassland)
