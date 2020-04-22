529fed6c4cd2a61d6fe994190cb009967acc7673
Kurt Krafft <kkrafft6609@gmail.com>
2020-04-22 14:22:39 -0500

    added biomes = 0

--- plants/list_environments_for_plants.py
+++ plants/list_environments_for_plants.py
@@ -25,17 +25,17 @@ def list_environments (locations, plant):
     # os.system('cls' if os.name == 'nt' else 'clear')
     if len(locations) == 0:
         print("****   No biomes found please try again  ****")
-        # sys.exit()
-
-    for location in locations:
-        print(f"{counter}. {location} ({len(location.plants)} plants)")
-        counter += 1
-    choice = input("Choose your environment > ")
-    num = int(choice)-1
-    if len(locations[num].plants) == locations[num].plant_capacity:
-        print("****   That biome is not large enough   ****")
-        print("****     Please choose another one      ****")
-        list_environments(locations, plant)
+        
     else:
-        locations[num].plants.append(plant)
+        for location in locations:
+            print(f"{counter}. {location} ({len(location.plants)} plants)")
+            counter += 1
+        choice = input("Choose your environment > ")
+        num = int(choice)-1
+        if len(locations[num].plants) == locations[num].plant_capacity:
+            print("****   That biome is not large enough   ****")
+            print("****     Please choose another one      ****")
+            list_environments(locations, plant)
+        else:
+            locations[num].plants.append(plant)
     
