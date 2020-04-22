695780687ba3c85b0aafdecd04e3472b957c5666
Jack Parsons <jcksnparsons@yahoo.com>
2020-04-22 15:05:21 -0500

    refactored yet again

--- actions/report.py
+++ actions/report.py
@@ -3,17 +3,17 @@ from arboretum import Arboretum
 def build_facility_report(Arboretum):
     for environments in Arboretum.master:
         for environment in environments:
-            print(f'''     {environment}''')
+            print(environment)
             print('''
                     ----- ANIMALS -----
             ''')
             for animal in environment.animals:
-                print(animal)
+                print(f'        {animal}')
             print('''
                     ----- PLANTS ------
              ''')
             for plant in environment.plants:
-                print(plant)
+                print(f'        {plant}')
             
     print('''
                 ------  END REPORT  -------
