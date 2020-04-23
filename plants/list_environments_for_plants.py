8259268ba62d04cdf996d4724233065e4c0af504
Kurt Krafft <kkrafft6609@gmail.com>
2020-04-23 10:06:57 -0500

    trys and excepts addition

--- plants/list_environments_for_plants.py
+++ plants/list_environments_for_plants.py
@@ -32,14 +32,18 @@ def list_environments (locations, plant):
             print(f"{counter}. {location} ({len(location.plants)} plants)")
             counter += 1
         choice = input("Choose your environment > ")
+        print(type(int(choice)))
+
         try:
-            num = int(choice)-1
-            if len(locations[num].plants) == locations[num].plant_capacity:
-                print("****   That biome is not large enough   ****")
-                print("****     Please choose another one      ****")
-                list_environments(locations, plant)
-            else:
-                locations[num].plants.append(plant)
+            if type(int(choice)) == 'int':
+                num = int(choice)-1
+                if len(locations[num].plants) == locations[num].plant_capacity:
+                    print("****   That biome is not large enough   ****")
+                    print("****     Please choose another one      ****")
+                    list_environments(locations, plant)
+                else:
+                    locations[num].plants.append(plant)
+                    input("\n\nPress any key to continue...")
         except TypeError:
             print("Please enter a number...")
             input("\n\nPress any key to continue...")
