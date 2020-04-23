50f668e3939290899a5f819a83c2cc7013e2a3bd
Kurt Krafft <kkrafft6609@gmail.com>
2020-04-23 10:26:40 -0500

    flants 1

--- plants/list_environments_for_plants.py
+++ plants/list_environments_for_plants.py
@@ -34,18 +34,32 @@ def list_environments (locations, plant):
         choice = input("Choose your environment > ")
         print(type(int(choice)))
 
-        try:
-            if type(int(choice)) == 'int':
-                num = int(choice)-1
-                if len(locations[num].plants) == locations[num].plant_capacity:
-                    print("****   That biome is not large enough   ****")
-                    print("****     Please choose another one      ****")
-                    list_environments(locations, plant)
-                else:
-                    locations[num].plants.append(plant)
-                    input("\n\nPress any key to continue...")
-        except TypeError:
-            print("Please enter a number...")
+        
+        # num = int(choice)-1
+        # if len(locations[num].plants) == locations[num].plant_capacity:
+        #     print("****   That biome is not large enough   ****")
+        #     print("****     Please choose another one      ****")
+        #     list_environments(locations, plant)
+        # else:
+        #     locations[num].plants.append(plant)
+        #     print(f"The {plant.species} was added to {locations[num]}")
+        #     input("\n\nPress any key to continue...")
+
+
+        
+        if type(int(choice)) is int:
+            num = int(choice)-1
+            if len(locations[num].plants) == locations[num].plant_capacity:
+                print("****   That biome is not large enough   ****")
+                print("****     Please choose another one      ****")
+                list_environments(locations, plant)
+            else:
+                locations[num].plants.append(plant)
+                print(f"The {plant.species} was added to {locations[num]}")
+                input("\n\nPress any key to continue...")
+        else:
+            raise TypeError("Please Enter a number...")
+            # print("Please enter a number...")
             input("\n\nPress any key to continue...")
 
 
