7d7f9e8e839cd008bf7a3e416aaa09498a305b37
Kurt Krafft <kkrafft6609@gmail.com>
2020-04-23 09:59:44 -0500

    whoops

--- plants/list_environments_for_plants.py
+++ plants/list_environments_for_plants.py
@@ -32,11 +32,17 @@ def list_environments (locations, plant):
             print(f"{counter}. {location} ({len(location.plants)} plants)")
             counter += 1
         choice = input("Choose your environment > ")
-        num = int(choice)-1
-        if len(locations[num].plants) == locations[num].plant_capacity:
-            print("****   That biome is not large enough   ****")
-            print("****     Please choose another one      ****")
-            list_environments(locations, plant)
-        else:
-            locations[num].plants.append(plant)
+        try:
+            num = int(choice)-1
+            if len(locations[num].plants) == locations[num].plant_capacity:
+                print("****   That biome is not large enough   ****")
+                print("****     Please choose another one      ****")
+                list_environments(locations, plant)
+            else:
+                locations[num].plants.append(plant)
+        except TypeError:
+            print("Please enter a number...")
+            input("\n\nPress any key to continue...")
+
+
     
